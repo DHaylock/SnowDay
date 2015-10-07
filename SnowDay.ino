98a17573eb0229ed07273cdfe45e1a3abb5d53ff
David Haylock <david.haylock@watershed.co.uk>
2015-10-07 11:18:17 +0100

    - SnowDay now uses neopixels

--- SnowDay.ino
+++ SnowDay.ino
@@ -1,6 +1,13 @@
+#include "neopixel.h"
 #include "rest_client.h"
 
 RestClient client = RestClient("");
+
+#define PIXEL_PIN D2
+#define PIXEL_COUNT 16
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 int photoresistor = A0;
 int pwr = A5;
@@ -11,8 +18,11 @@ char value[5];
 
 //---------------------------------------------------
 void setup() {
+
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+    Spark.publish("Start up", "Starting",60,PRIVATE);
     Serial.begin(9600);
-    // Spark.connect();
     pinMode(led, OUTPUT);
     pinMode(photoresistor,INPUT);
     pinMode(pwr,OUTPUT);
@@ -22,15 +32,20 @@ void setup() {
 void loop() {
     resistorValue = analogRead(photoresistor);
 
-    if(resistorValue < 10){
-        digitalWrite(led, HIGH);
+    if(resistorValue < 100){
+
         if(getTweets == false) {
             Spark.publish("Flag", "Lock",60,PRIVATE);
             get();
         }
     }
-    else if(resistorValue > 11) {
-        digitalWrite(led, LOW);
+    else if(resistorValue > 101 && getTweets) {
+        digitalWrite(led,LOW);
+        for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,0));
+        }
+        strip.show();
+
         if(getTweets == true) {
             Spark.publish("Flag", "Unlock",60,PRIVATE);
             getTweets = false;
@@ -40,7 +55,8 @@ void loop() {
 
     }
     sprintf(value,"%d",resistorValue);
-    // Spark.publish("LDR", value,60,PRIVATE);
+    //  For Debug Purposes only
+    //  Spark.publish("Resistor",value,60,PRIVATE);
     delay(500);
 }
 //---------------------------------------------------
@@ -50,5 +66,16 @@ void get() {
     Serial.println(statusCode);
     Serial.println(response);
     Spark.publish("Number of Tweets",response,60,PRIVATE);
+    if(response.toInt() > 0) {
+        digitalWrite(led,HIGH);
+         for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,255));
+        }
+        strip.show();
+    }
+    else{
+        digitalWrite(led,LOW);
+    }
+
     getTweets = true;
 }
